e9a23dc263a9b1d7268ec8667a8ee22389ba57ef
Giovani Marciniak <giovani.ggm@gmail.com>
2019-11-12 10:38:27 -0300

    o trabalho podre

--- edp_lib.c
+++ edp_lib.c
@@ -1,7 +1,7 @@
 /**
 *	@file edp_lib.c
 *	@author GRR20163049 Bruno Henrique Labres
-*	@author GRR20182981 Giovani Gurkevicz Marciniak 
+*	@author GRR20182981 Giovani Gurkevicz Marciniak
 *	@date 6 de Outubro de 2019
 *	@brief Biblioteca com as funções envolvendo equações diferenciais parciais.
 */
@@ -23,7 +23,7 @@ real_t timestamp(void)
 
 
 /**
-*  @brief Essa função cria um estrutura de dados necessarias para o calculo de EDPs (Equação Diferencial Parcial) 
+*  @brief Essa função cria um estrutura de dados necessarias para o calculo de EDPs (Equação Diferencial Parcial)
 *  @param nx Número de pontos no eixo x da malha
 *  @param ny Número de pontos no eixo y da malha
 *  @param lx Limite do eixo x na malha
@@ -86,12 +86,12 @@ FILE * trataArgumentos(int argc, char *argv[], int *nx, int *ny, int *maxIter){
 			*nx = atoi(argv[i+1]);
 			contador_arg++;
 		}
-		
+
 		if (!strcmp(argv[i], "-ny")){
 			*ny = atoi(argv[i+1]);
 			contador_arg++;
 		}
-	
+
 		if (!strcmp(argv[i], "-i")){
 			*maxIter = atoi(argv[i+1]);
 			contador_arg++;
@@ -128,9 +128,9 @@ void calculaMatrizCoef(EDP_t *e){
 		yi += hy;
 		xi = 0.0;
 		for(i = 0; i < e->nx ; i++) {
-			xi += hx;	
+			xi += hx;
 			e->b[j*e->nx+i] = (2*hx*hx*hy*hy)*F(xi,yi); // calcula vetor de termos independentes
-	 	
+
 			if(j == 0){
 				// aplica condicao de fronteira
 				e->b[j*e->nx+i] -= e->dia*sin(2*PI*(PI - xi))*sinh(PI*PI);
@@ -171,39 +171,59 @@ real_t calculaGaussSeidel(EDP_t *e, real_t *r){
 	unsigned int pos;
 
 	//double tempoInicio, somaTempo;
-	
+
 // #### new gauss ####
 
 for (unsigned int iter = 0 ; iter < e->maxIter ; iter++) {
-
 	// calcula por fora o valor da "matriz" pos[0,0]
 	e->x[0] = e->b[0] - e->ds*e->x[1] - e->dsa*e->x[e->nx];
 	// calcula a primeira linha da "matriz"
 	for (unsigned int i = 1 ; i <= e->nx-2 ; i++) {
-		e->x[i] = e->b[i] - e->di*e->x[i-1] 
-		real_t aux = e->ds*e->x[i+1] - e->dsa*e->x[i+e->nx];
+		e->x[i] = e->b[i] - e->di*e->x[i-1];
+		real_t aux = e->ds*e->x[i+1] + e->dsa*e->x[i+e->nx];
 		e->x[i] -= aux;
 	}
 	// calcula por fora o ultimo valor da "matriz" pos[0,nx]
 	e->x[e->nx-1] = e->b[e->nx-1] - e->di*e->x[e->nx-2] - e->dsa*e->x[2*e->nx-1];
 
-	// ## calcula a "matriz" interna ##
-
-	for (unsigned int j = 1 ; j <= e->ny -2 ; j++) {
-		
-		for (i = 1 ; i <= e->nx -2 ; i ++) {
-
+	// calcula a "matriz" interna
+  for (unsigned int j = 1 ; j <= e->ny -2 ; j++) {
+     // calculando o primeiro valor
+      unsigned int k = j*e->nx; // só pra não calcular toda vez
+      e->x[k] = e->b[k] - e->ds*e->x[k+1];
+      real_t aux = e->dia*e->x[k-e->nx] + e->dsa*e->x[k+e->nx];
+      e->x[k] -= aux;
+      // calculando valores centrais
+      for (unsigned int i = 1 ; i <= e->nx -2 ; i ++) {
+        e->x[k+i] = e->b[k+i] - e->di*e->x[k+i-1];
+        aux = e->ds*e->x[k+i+1] +  e->dia*e->x[k+i-e->nx];
+        e->x[k+i] = e->x[k+i] - aux - e->dsa*e->x[k+i+e->nx];
 		}
+    // calculando o ultimo valor
+    e->x[k+e->nx-1] = e->b[k+e->nx-1] - e->di*e->x[k+e->nx-2];
+    aux = e->dia*e->x[k-1] + e->dsa*e->x[k+2*e->nx-1];
+    e->x[k+e->nx-1] = e->x[k+e->nx-1] - aux;
 	}
 
+  // calcula pos[0,ny-1]
+  unsigned int k = e->nx*(e->ny-1); // só pra calcular menos
+  e->x[k] = e->b[k] - e->ds*e->x[k+1] - e->dia*e->x[k-e->nx];
+  // calcula última linha da matriz "fronteira"
+  for (unsigned int i = 1 ; i <= e->nx-2 ; i++) {
+		e->x[k+i] = e->b[k+i] -  e->di*e->x[k-1];
+		real_t aux = e->ds*e->x[k+i+1] + e->dia*e->x[k+i-e->nx];
+		e->x[i] -= aux;
+	}
+  // calcula pos[nx-1,ny-1]
+  e->x[(e->nx)*(e->ny)-1] = e->b[(e->nx)*(e->ny)-1] - e->di*e->x[(e->nx)*(e->ny)-2] - e->dia*e->x[(e->nx)*(e->ny)-1-e->nx];
 }
 
 // ### end new gauss ###
-
+/*
 	double somaTempo = 0.0;
 
 	for(iter = 0 ; iter < e->maxIter ; iter++)
-	{			
+	{
 		double tempoInicio = timestamp();
 
 		for(j = 0 ; j < e->ny ; j++){
@@ -211,31 +231,32 @@ for (unsigned int iter = 0 ; iter < e->maxIter ; iter++) {
 				pos = j*e->nx+i; // iterar pelo vetor solucao
 
 				e->x[pos] = e->b[pos];
-				
+
 				// analisa os casos de fronteira
 				if(i != 0)    e->x[pos] -= e->di*e->x[pos-1];
 				if(i != (e->nx-1)) e->x[pos] -= e->ds*e->x_prev[pos+1];
-				if(j != 0)    e->x[pos] -= e->dia*e->x[pos-e->nx]; //este nop
+				if(j != 0)    e->x[pos] -= e->dia*e->x[pos-e->nx];
 				if(j != (e->ny-1)) e->x[pos] -= e->dsa*e->x_prev[pos+e->nx];
-				
+
 				e->x[pos] = e->x[pos]/e->dp; // guarda valor de z na malha
-				
+
 				e->x_prev[pos] = e->x[pos]; // guarda it anterior
 			}
-		}				
+		}
 
 		somaTempo += (timestamp() - tempoInicio);
 		r[iter] = calculaResiduo(e);
 
 	}
-	
+*/
 
-	return somaTempo/e->maxIter;
+	//return somaTempo/e->maxIter;
+  return 1.0;
 }
 
 
 /**
-*  \brief Essa função calcula do resíduo de uma iteração do GaussSeidel 
+*  \brief Essa função calcula do resíduo de uma iteração do GaussSeidel
 *  \param e Estrutura de dados com as variaveis para os calculos envolvendo EDPs
 *  \return sqrt(normaquadrada) a norma L2 da iteração
 */
@@ -246,19 +267,19 @@ real_t calculaResiduo(EDP_t *e) {
 
 	for(j = 0 ; j < e->ny ; j++){
 		for(i = 0; i < e->nx; i++){
-			pos = j*e->nx+i; // iterar pelo vetor 
-			
+			pos = j*e->nx+i; // iterar pelo vetor
+
 			residuo = (e->b[pos]) - (e->dp*e->x[pos]);
-		
+
 			//analisa os casos de fronteira
 			if(i != 0)    residuo -= e->di * e->x[pos-1];
 			if(i != (e->nx-1)) residuo -= e->ds * e->x[pos+1];
 			if(j != 0)    residuo -= e->dia * e->x[pos-e->nx];
 			if(j != (e->ny-1)) residuo -= e->dsa * e->x[pos+e->nx];
-			
+
 			normaquadrada += residuo * residuo;
 		}
-	}				
+	}
 
 	return sqrt(normaquadrada);
 
